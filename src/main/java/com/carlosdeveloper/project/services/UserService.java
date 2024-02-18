19c83f6801a23c44e16fc382640d3d67f903104b
carloshipol <carlaogaruda30@gmail.com>
2024-02-18 17:01:53 -0300

    feat: Service layer, component registration

---
+++ src/main/java/com/carlosdeveloper/project/services/UserService.java
@@ -0,0 +1,27 @@
+package com.carlosdeveloper.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carlosdeveloper.project.entities.User;
+import com.carlosdeveloper.project.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
